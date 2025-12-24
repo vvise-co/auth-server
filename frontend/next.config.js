bfd90afe188eda11fc02025359237afc4331af9b
BMahmoudi <bashir.mahmoudi@gmail.com>
2025-12-24 17:47:57 +0000

    next js build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 module.exports = nextConfig
